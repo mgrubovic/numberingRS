ffc887dbacab8bbab3182f6a3ec70e458c336bdc
mgrubovic <35999510+mgrubovic@users.noreply.github.com>
2019-06-06 16:08:53 +0200

    Add files via upload

--- src/rs/numbering/control/OutputDataCtrlServlet.java
+++ src/rs/numbering/control/OutputDataCtrlServlet.java
@@ -12,8 +12,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import rs.numbering.format.Range;
 import rs.numbering.jaxb.OperationJaxb;
+import rs.numbering.json.simple.RangeJsonSimple;
 
 /**
  * Servlet implementation class OutputDataCtrlServlet
@@ -24,9 +27,7 @@ public class OutputDataCtrlServlet extends HttpServlet {
 	
 	public OutputDataCtrlServlet() {
 		super();
-
 	}
-
 	private static final long serialVersionUID = 54L;
        
     /**
@@ -35,17 +36,41 @@ public class OutputDataCtrlServlet extends HttpServlet {
 
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String responseTo= "/numbers/CheckNumber.jsp";
     	ServletContext sContext = request.getServletContext();
-    	String filePath = sContext.getRealPath("/data");
+    	String dataFolder = "/data";
+    	String filePath = sContext.getRealPath(dataFolder) + File.separator;
+
     	List <Range> rangesMain = (List<Range>) sContext.getAttribute("geoRange");
 
+    	String type = request.getParameter("file");
+    	if(type.equals("json")){
+    		responseTo = makeJsonFile(rangesMain, filePath);
+    	}else if (type.equals("xml")){
+    		responseTo = makeXmlFile(rangesMain, filePath);
+    	}
+    	RequestDispatcher view = request.getRequestDispatcher(responseTo);
+		view.forward(request, response);
+	}
+
+	private String makeXmlFile(List <Range> rangesMain, String filePath ) {
     	String fileXmlName = "RangesList.xml";
-		String fileXmlPlace = filePath + File.separator +  fileXmlName;
+		String fileXmlPlace = filePath +fileXmlName;
 		File writeRange = new File(fileXmlPlace);
 		OperationJaxb.listRangeToXml(rangesMain, writeRange);
+		return "/data/RangesList.xml";
+	}
+
+	private String makeJsonFile(List <Range> rangesMain, String filePath) {
+		System.out.println("You click on Json file");
+    	String fileJsonName = "RangesList.json";
+		String fileJsonPlace = filePath +fileJsonName;
+		RangeJsonSimple rjs= new RangeJsonSimple();
+		JSONObject jo = rjs.prepareJson(rangesMain);
+		rjs.jsonToFile(jo, fileJsonPlace);
+		return "/data/RangesList.json";
+
 		
-		RequestDispatcher view = request.getRequestDispatcher("/data/RangesList.xml");
-		view.forward(request, response);
 	}
 
 	@Override
